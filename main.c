36398c0057615cdb51d7f21207bb6dc0a78809f4
Thomas McLaughlin <thomas.mclaughlin2@stvincent.edu>
2023-06-13 18:41:03 -0400

    Refactor parse_tokenize and read_validate in main.

--- main.c
+++ main.c
@@ -3,27 +3,28 @@
 char *read_validate_line(void);
 
 //parse the line into a list of arguments
-char *parse_tokenize_line(char *userInputLine);
+char **parse_tokenize_line(char *userInputLine);
 
-int main(int argumentCount, char **argumentVector){
-    char *initPrompt = "t-shell> $ ";
-    
+int main(int argumentCount, char **argumentVector){    
     char *userInputLine;
-    char *tokenizedLine;
+    char **tokenizedLine;
 
-    (void)argumentCount; (void)argumentVector;(void)initPrompt;
+    (void)argumentCount; (void)argumentVector;
 
+    printf("t-shell> $ ");
     userInputLine = read_validate_line();
     tokenizedLine = parse_tokenize_line(userInputLine);
 
     free(userInputLine);
+    free(tokenizedLine);
+
     return (0);
 }
 
 char *read_validate_line(void){
     int initBufferSize = 1024; //seems as if this is the norm
     char *curBuffer = malloc(sizeof(char) * initBufferSize);
-    if (!curBuffer) { //curBuffer was unable to allocate the memory if it is null
+    if (!curBuffer) { //curBuffer was unable to al`locate the memory if it is null
         fprintf(stderr, "There was an allocation error you fool!\n");
         exit(EXIT_FAILURE);
     }
@@ -55,6 +56,36 @@ char *read_validate_line(void){
         }
     }
 }
-char *parse_tokenize_line(char *userInputLine){
+char **parse_tokenize_line(char *userInputLine){
+    int initBufferSize = 64; // Seems to be what everyone else is doing...
+    char **tokenCollection = malloc(sizeof(char ) * initBufferSize );
+    char *curToken;
 
+    int i = 0;
+
+    if (!tokenCollection) {
+        fprintf(stderr, "There was an allocation error you fool!\n");
+        exit(EXIT_FAILURE);
+    }
+    curToken = strtok(userInputLine, " \t\n\r\a"); // We tokenize the line based on whitespace
+    while(curToken != NULL){
+        tokenCollection[i] = curToken;
+        i++;
+
+        //checking to see if the current token count is past our current buffer size
+        if (i >= initBufferSize) {
+            initBufferSize += initBufferSize;
+            curToken = realloc(tokenCollection, initBufferSize * sizeof(char*));
+            if (!tokenCollection) {
+                fprintf(stderr, "There was an allocation error you fool!\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        curToken = strtok(NULL, " \t\n\r\a");
+    }
+
+    tokenCollection[i] = NULL;
+    printf("t-shell> $ ");
+
+    return tokenCollection;
 }
